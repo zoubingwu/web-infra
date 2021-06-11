bd90f72580c9830c62c999f79203a02a025e1b8e
zoubingwu <zoubingwu@gmail.com>
2021-06-11 13:01:23 +0800

    feat(create-app): add react-ts-vite template

--- packages/create-app/index.js
+++ packages/create-app/index.js
@@ -2,9 +2,10 @@
 
 const fs = require('fs')
 const path = require('path')
+const execSync = require('child_process').execSync
 const argv = require('minimist')(process.argv.slice(2))
-const { prompt } = require('enquirer')
-const { cyan, blue } = require('chalk')
+const { cyan, blue, yellow, green } = require('chalk')
+const prompts = require('prompts')
 
 const cwd = process.cwd()
 
@@ -17,6 +18,11 @@ const FRAMEWORKS = [
         name: 'react-ts',
         display: 'TypeScript',
         color: blue,
+      },
+      {
+        name: 'react-ts-vite',
+        display: 'TypeScript',
+        color: yellow,
       },
     ],
   },
@@ -39,32 +45,19 @@ function copy(src, dest) {
   }
 }
 
-async function getValidPackageName(projectName) {
-  const packageNameRegExp =
-    /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/
-  if (packageNameRegExp.test(projectName)) {
-    return projectName
-  } else {
-    const suggestedPackageName = projectName
-      .trim()
-      .toLowerCase()
-      .replace(/\s+/g, '-')
-      .replace(/^[._]/, '')
-      .replace(/[^a-z0-9-~]+/g, '-')
+function isValidPackageName(projectName) {
+  return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(
+    projectName
+  )
+}
 
-    /**
-     * @type {{ inputPackageName: string }}
-     */
-    const { inputPackageName } = await prompt({
-      type: 'input',
-      name: 'inputPackageName',
-      message: `Package name:`,
-      initial: suggestedPackageName,
-      validate: input =>
-        packageNameRegExp.test(input) ? true : 'Invalid package.json name',
-    })
-    return inputPackageName
-  }
+function toValidPackageName(projectName) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/^[._]/, '')
+    .replace(/[^a-z0-9-~]+/g, '-')
 }
 
 function copyDir(srcDir, destDir) {
@@ -92,61 +85,139 @@ function emptyDir(dir) {
   }
 }
 
+function isEmpty(path) {
+  return fs.readdirSync(path).length === 0
+}
+
+function isInGitRepository(cwd) {
+  try {
+    execSync('git rev-parse --is-inside-work-tree', { stdio: 'ignore', cwd })
+    return true
+  } catch (e) {
+    return false
+  }
+}
+
+function tryGitInit(cwd) {
+  try {
+    execSync('git --version', { stdio: 'ignore', cwd })
+    if (isInGitRepository(cwd)) {
+      return false
+    }
+
+    execSync('git init', { stdio: 'ignore', cwd })
+    return true
+  } catch (e) {
+    console.warn('Git repo not initialized', e)
+    return false
+  }
+}
+
 ;(async function () {
   let targetDir = argv._[0]
-  if (!targetDir) {
-    const { projectName } = await prompt({
-      type: 'input',
-      name: 'projectName',
-      message: `Project name:`,
-      initial: 'my-project',
-    })
-    targetDir = projectName
-  }
-  const packageName = await getValidPackageName(targetDir)
-  const root = path.join(cwd, targetDir)
+  let template = argv.template || argv.t
 
-  if (!fs.existsSync(root)) {
-    fs.mkdirSync(root, { recursive: true })
-  } else {
-    const existing = fs.readdirSync(root)
-    if (existing.length) {
-      /**
-       * @type {{ yes: boolean }}
-       */
-      const { yes } = await prompt({
-        type: 'confirm',
-        name: 'yes',
-        initial: 'Y',
-        message:
-          (targetDir === '.'
-            ? 'Current directory'
-            : `Target directory ${targetDir}`) +
-          ' is not empty.\n' +
-          'Remove existing files and continue?',
-      })
-      if (yes) {
-        emptyDir(root)
-      } else {
-        return
+  const defaultProjectName = !targetDir ? 'my-project' : targetDir
+  let result
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: targetDir ? null : 'text',
+          name: 'projectName',
+          message: 'Project name:',
+          initial: defaultProjectName,
+          onState: state =>
+            (targetDir = state.value.trim() || defaultProjectName),
+        },
+
+        {
+          type: () =>
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : 'confirm',
+          name: 'overwrite',
+          message: () =>
+            (targetDir === '.'
+              ? 'Current directory'
+              : `Target directory "${targetDir}"`) +
+            ` is not empty. Remove existing files and continue?`,
+        },
+
+        {
+          type: (_, { overwrite } = {}) => {
+            if (overwrite == false) {
+              throw new Error(red('✖') + ' Operation cancelled')
+            }
+            return null
+          },
+          name: 'overwriteChecker',
+        },
+
+        {
+          type: () => (isValidPackageName(targetDir) ? null : 'text'),
+          name: 'packageName',
+          message: 'Package name:',
+          initial: () => toValidPackageName(targetDir),
+          validate: dir =>
+            isValidPackageName(dir) || 'Invalid package.json name',
+        },
+
+        {
+          type: template && TEMPLATES.includes(template) ? null : 'select',
+          name: 'framework',
+          message:
+            template && !TEMPLATES.includes(template)
+              ? `"${template}" isn't a valid template. Please choose from below: `
+              : 'Select a framework:',
+          initial: 0,
+          choices: FRAMEWORKS.map(framework => {
+            const frameworkColor = framework.color
+            return {
+              title: frameworkColor(framework.name),
+              value: framework,
+            }
+          }),
+        },
+
+        {
+          type: framework =>
+            framework && framework.variants ? 'select' : null,
+          name: 'variant',
+          message: 'Select a variant:',
+          // @ts-ignore
+          choices: framework =>
+            framework.variants.map(variant => {
+              const variantColor = variant.color
+              return {
+                title: variantColor(variant.name),
+                value: variant.name,
+              }
+            }),
+        },
+      ],
+
+      {
+        onCancel: () => {
+          throw new Error(red('✖') + ' Operation cancelled')
+        },
       }
-    }
+    )
+  } catch (e) {
+    console.log(cancelled.message)
+    return
   }
 
-  let template = argv.t || argv.template
-  let message = 'Select a framework:'
-  let isValidTemplate = false
+  const { framework, overwrite, packageName, variant } = result
+  const root = path.join(cwd, targetDir)
+  template = variant || framework || template
 
-  if (typeof template === 'string') {
-    isValidTemplate = TEMPLATES.includes(template)
-    message = `${template} isn't a valid template. Please choose from below:`
+  if (overwrite) {
+    emptyDir(root)
+  } else if (!fs.existsSync(root)) {
+    fs.mkdirSync(root)
   }
 
-  if (!template || !isValidTemplate) {
-    template = 'react-ts'
-  }
-
-  console.log(`\nScaffolding project in ${root}...`)
+  console.log(`\nScaffolding project in ${green(root)}...`)
 
   const templateDir = path.join(__dirname, `template-${template}`)
 
@@ -172,11 +243,15 @@ function emptyDir(dir) {
 
   write('package.json', JSON.stringify(pkg, null, 2))
 
+  if (tryGitInit(targetDir)) {
+    console.log(`Initialized a git repository`)
+  }
+
   console.log(`\nDone. You can run following commands to start:\n`)
   if (root !== cwd) {
     console.log(`  cd ${path.relative(cwd, root)}`)
   }
-  console.log(`  yarn && yarn prepare`)
+  console.log(`  yarn`)
   console.log(`  yarn start`)
   console.log()
 })()
