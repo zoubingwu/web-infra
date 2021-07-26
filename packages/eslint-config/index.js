b62010928e9a632cf34c7c183da660df1269d909
zoubingwu <zoubingwu@gmail.com>
2021-07-26 17:21:17 +0800

    feat(eslint-config): added import plugin for import ordering

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,4 +1,22 @@
 module.exports = {
   extends: ['alloy', 'alloy/react', 'alloy/typescript'],
   parser: '@typescript-eslint/parser',
+  plugins: ['import'],
+  root: true,
+
+  // override rules here
+  rules: {
+    '@typescript-eslint/method-signature-style': 'off',
+
+    /**
+     * @reason a more strict import order for better maintainability
+     */
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object'],
+        'newlines-between': 'always',
+      },
+    ],
+  },
 }
