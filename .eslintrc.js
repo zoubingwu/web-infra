6eb02be33a8b437195ecdfeae53edef0eb001f27
zoubingwu <zoubingwu@gmail.com>
2021-07-26 16:44:57 +0800

    chore: add eslint to root

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ['./packages/eslint-config'],
+  env: {
+    // Your custom env variables, e.g., browser: true, jest: true
+  },
+  globals: {
+    // Your global variables
+  },
+  rules: {
+    // Your custom rules.
+  },
+}
