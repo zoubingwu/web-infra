1c2506150925c1e889aa38116b419ed9e03a9970
zoubingwu <zoubingwu@gmail.com>
2021-08-02 19:58:11 +0800

    release(eslint-config): v0.3.0

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -10,7 +10,17 @@ module.exports = {
      * @link https://github.com/typescript-eslint/typescript-eslint/blob/15f718415eb4e522440a5d81f38d65ee81535795/packages/eslint-plugin/docs/rules/method-signature-style.md
      */
     '@typescript-eslint/method-signature-style': 'off',
+
+    /**
+     * @link https://github.com/typescript-eslint/typescript-eslint/blob/99eca0d428187d4c29ded9ddd1b57b40ab463c01/packages/eslint-plugin/docs/rules/no-invalid-void-type.md
+     */
     '@typescript-eslint/no-invalid-void-type': 'off',
+
+    /**
+     * @link https://github.com/typescript-eslint/typescript-eslint/blob/84fff3575282f09683385e855c20df3c261ce313/packages/eslint-plugin/docs/rules/consistent-type-definitions.md
+     * @reason we accept both type alias and interface
+     */
+    '@typescript-eslint/consistent-type-definitions': 'off',
 
     /**
      * @link https://github.com/import-js/eslint-plugin-import/blob/95e60112bcb116c14f18005c42adf3d3e4501b32/docs/rules/order.md
@@ -31,11 +41,5 @@ module.exports = {
         'newlines-between': 'always',
       },
     ],
-
-    /**
-     * @link https://github.com/typescript-eslint/typescript-eslint/blob/84fff3575282f09683385e855c20df3c261ce313/packages/eslint-plugin/docs/rules/consistent-type-definitions.md
-     * @reason we accept both type alias and interface
-     */
-    '@typescript-eslint/consistent-type-definitions': 'off',
   },
 }
