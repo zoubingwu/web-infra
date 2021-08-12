b09c213afe033942db29016e935eff4ee448ce1f
zoubingwu <zoubingwu@gmail.com>
2021-08-12 17:56:08 +0800

    feat(site): add crawler to generate nav menu from mdx docs

---
+++ packages/site/scripts/crawler.mjs
@@ -0,0 +1,78 @@
+import * as fs from "fs";
+import * as path from "path";
+import { fileURLToPath } from 'url';
+
+import {VFile} from 'vfile'
+import {visit} from 'unist-util-visit'
+import matter from 'gray-matter';
+// @ts-ignore
+import mdx from '@mdx-js/mdx' // FIXME: mdx-2.0.0-next.9 missing some types, upgrade it as soon as newer version released
+
+const filename = fileURLToPath(import.meta.url)
+const SITE_ROOT = path.resolve(path.dirname(filename), '..')
+const DOCS_DIR = path.resolve(SITE_ROOT, './docs')
+const SRC_DIR = path.resolve(SITE_ROOT, './src')
+const DOCS_ENTRY = fs.readdirSync(DOCS_DIR)
+const docsData = {
+  nav: []
+}
+DOCS_ENTRY.forEach(doc => {
+  const { content, data } = matter.read(path.resolve(DOCS_DIR, doc))
+  const title = data.title || path.basename(doc, '.mdx')
+  const item = {
+    title,
+    index: data.index,
+    children: [],
+    level: 0,
+    route: toRoute(title.toLowerCase()),
+  }
+
+  const children = []
+  const file = new VFile(content)
+  const extractTitle = () => tree => visit(tree, 'heading', (node) => {
+        if (node.depth > 2) return
+
+        const value = node.children[0].value
+        if (node.depth === 1) {
+          children.push(createItem(value, node.depth, toRoute(value)))
+        } else if (node.depth === 2) {
+          const parentTitle = findParentTitle(children, node.depth - 1)
+          if (parentTitle) {
+            children.push(createItem(value, node.depth, parentTitle + '.' + toRoute(value)))
+          } else {
+            children.push(createItem(value, node.depth, toRoute(value)))
+          }
+        }
+      })
+
+  const mdxCompiler = mdx.createCompiler({
+    remarkPlugins: [extractTitle]
+  })
+
+  mdxCompiler.processSync(file)
+  docsData.nav.push(item)
+})
+
+function findParentTitle(titles, level) {
+  for (let i = titles.length; i--; i >= 0) {
+    if (titles[i].level === level) {
+      return titles[i].route
+    }
+  }
+  return null
+}
+
+function createItem(title, level, route) {
+  return {
+    title,
+    level,
+    route,
+  }
+}
+
+function toRoute(val) {
+  return val.toLowerCase().split(' ').join('-')
+}
+
+fs.writeFileSync(path.resolve(SRC_DIR, './docs.generated.json'), JSON.stringify(docsData, null, 2))
+console.log('docs json generated!')
--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -1,20 +1,11 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import ssr from 'vite-plugin-ssr/plugin'
 import windicss from 'vite-plugin-windicss'
+import mdx from 'vite-plugin-mdx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    reactRefresh(),
-    ssr(),
-    windicss({
-      scan: {
-        dirs: ['pages'],
-        fileExtensions: ['ts', 'tsx', 'html'],
-      },
-    }),
-  ],
+  plugins: [reactRefresh(), windicss(), mdx()],
   base: '/web-infra/',
   define: {
     'process.env': {},
