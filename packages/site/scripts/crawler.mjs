3c37ea44569488546c0d3f1009a40f780337aa55
zoubingwu <zoubingwu@gmail.com>
2021-08-12 18:03:45 +0800

    fix(site): fix typo & run fmt against mjs file

--- packages/site/scripts/crawler.mjs
+++ packages/site/scripts/crawler.mjs
@@ -1,12 +1,12 @@
-import * as fs from "fs";
-import * as path from "path";
-import { fileURLToPath } from 'url';
+import * as fs from 'fs'
+import * as path from 'path'
+import { fileURLToPath } from 'url'
 
-import {VFile} from 'vfile'
-import {visit} from 'unist-util-visit'
-import matter from 'gray-matter';
+import { VFile } from 'vfile'
+import { visit } from 'unist-util-visit'
+import matter from 'gray-matter'
 // @ts-ignore
-import mdx from '@mdx-js/mdx' // FIXME: mdx-2.0.0-next.9 missing some types, upgrade it as soon as newer version released
+import mdx from '@mdx-js/mdx' // FIXME: mdx-2.0.0-next.9 is missing some types, upgrade it as soon as newer version released
 
 const filename = fileURLToPath(import.meta.url)
 const SITE_ROOT = path.resolve(path.dirname(filename), '..')
@@ -14,7 +14,7 @@ const DOCS_DIR = path.resolve(SITE_ROOT, './docs')
 const SRC_DIR = path.resolve(SITE_ROOT, './src')
 const DOCS_ENTRY = fs.readdirSync(DOCS_DIR)
 const docsData = {
-  nav: []
+  nav: [],
 }
 DOCS_ENTRY.forEach(doc => {
   const { content, data } = matter.read(path.resolve(DOCS_DIR, doc))
@@ -29,24 +29,27 @@ DOCS_ENTRY.forEach(doc => {
 
   const children = []
   const file = new VFile(content)
-  const extractTitle = () => tree => visit(tree, 'heading', (node) => {
-        if (node.depth > 2) return
+  const extractTitle = () => tree =>
+    visit(tree, 'heading', node => {
+      if (node.depth > 2) return
 
-        const value = node.children[0].value
-        if (node.depth === 1) {
+      const value = node.children[0].value
+      if (node.depth === 1) {
+        children.push(createItem(value, node.depth, toRoute(value)))
+      } else if (node.depth === 2) {
+        const parentTitle = findParentTitle(children, node.depth - 1)
+        if (parentTitle) {
+          children.push(
+            createItem(value, node.depth, parentTitle + '.' + toRoute(value))
+          )
+        } else {
           children.push(createItem(value, node.depth, toRoute(value)))
-        } else if (node.depth === 2) {
-          const parentTitle = findParentTitle(children, node.depth - 1)
-          if (parentTitle) {
-            children.push(createItem(value, node.depth, parentTitle + '.' + toRoute(value)))
-          } else {
-            children.push(createItem(value, node.depth, toRoute(value)))
-          }
         }
-      })
+      }
+    })
 
   const mdxCompiler = mdx.createCompiler({
-    remarkPlugins: [extractTitle]
+    remarkPlugins: [extractTitle],
   })
 
   mdxCompiler.processSync(file)
@@ -74,5 +77,8 @@ function toRoute(val) {
   return val.toLowerCase().split(' ').join('-')
 }
 
-fs.writeFileSync(path.resolve(SRC_DIR, './docs.generated.json'), JSON.stringify(docsData, null, 2))
+fs.writeFileSync(
+  path.resolve(SRC_DIR, './docs.generated.json'),
+  JSON.stringify(docsData, null, 2)
+)
 console.log('docs json generated!')
