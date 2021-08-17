1b600d9187b7c4b3314eb936da9438acd9fcd398
zoubingwu <zoubingwu@gmail.com>
2021-08-17 17:35:31 +0800

    feat(site): added windi.config.js

---
+++ packages/site/windi.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+  /* configurations... */
+  theme: {
+    extend: {
+      colors: {
+        black: '#10161A',
+        white: '#fff',
+        'dark-gray1': '#182026',
+        'dark-gray2': '#202B33',
+        'dark-gray3': '#293742',
+        'dark-gray4': '#30404D',
+        'dark-gray5': '#394B59',
+      },
+    },
+  },
+})
