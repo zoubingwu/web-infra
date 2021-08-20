539c9a3c4355caeb40a5bcab6e6c1215d15f21eb
zoubingwu <zoubingwu@gmail.com>
2021-08-20 12:56:26 +0800

    fix(site): set wrapFragments to true to fix error that multi theme code block wrapped by undefined tag

--- packages/site/vite.config.ts
+++ packages/site/vite.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
           remarkShikiTwoslash,
           {
             themes: ['github-dark', 'github-light'],
+            wrapFragments: true,
           },
         ],
       ],
